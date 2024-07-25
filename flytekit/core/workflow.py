10206396854294197a6b1e7a5f1dfce62cf61006
Mecoli1219 <michaellai901026@gmail.com>
2024-07-25 21:45:43 +0800

    lint & docstring
    Signed-off-by: Mecoli1219 <michaellai901026@gmail.com>

--- flytekit/core/workflow.py
+++ flytekit/core/workflow.py
@@ -296,7 +296,7 @@ class WorkflowBase(object):
 
     def execute(self, **kwargs):
         raise Exception("Should not be called")
-    
+
     def remote(self, **kwargs) -> FlyteFuture:
         return FlyteFuture(self, **kwargs)
 
